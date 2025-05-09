317c23c22f8d5f1be7fbfa7cd4b7b29646627642
eswarkumar-a <esawrkumar12345@gmail.com>
2025-05-09 09:59:46 +0530

    Update home-page.service.ts
    Updating the correct string

--- src/app/home/HomePage-Services/home-page.service.ts
+++ src/app/home/HomePage-Services/home-page.service.ts
@@ -20,7 +20,7 @@ export class HomePageService {
 //
   //Getting the Products from backend API
   getProducts():Observable<IProduct[]>{
-    let tempVar = this.http.get<IProduct[]>('https://quickkart-webservice20250508114638-ejfnauc9f7hme3gp.centralindia-01.azurewebsites.net/api/home/getproducts')
+    let tempVar = this.http.get<IProduct[]>('https://quickkart-webservice20250509034753-b3c5hybhfkbygegp.canadacentral-01.azurewebsites.net/api/home/getproducts')
     console.log(tempVar)
     return tempVar
   }

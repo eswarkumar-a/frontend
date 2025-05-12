bbd9264f091eb43f7b3ce3e781c1bfd5ca4b995d
eswarkumar-a <esawrkumar12345@gmail.com>
2025-05-12 06:13:45 +0000

    Update home-page.service.ts
    updated api gateway url

--- src/app/home/HomePage-Services/home-page.service.ts
+++ src/app/home/HomePage-Services/home-page.service.ts
@@ -20,7 +20,7 @@ export class HomePageService {
 //
   //Getting the Products from backend API
   getProducts():Observable<IProduct[]>{
-    let tempVar = this.http.get<IProduct[]>('https://quickkart-webservice20250509034753-b3c5hybhfkbygegp.canadacentral-01.azurewebsites.net/api/home/getproducts')
+    let tempVar = this.http.get<IProduct[]>('https://groupaapiii.azure-api.net/api/home/getproducts')
     console.log(tempVar)
     return tempVar
   }
